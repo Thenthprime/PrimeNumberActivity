1ce727a2004b8f968b920a26a912d4af12254c82
Thenthprime <122050458+Thenthprime@users.noreply.github.com>
2024-09-11 12:03:30 -0500

    Correct file placements and add missing files

---
+++ PrimeNumber/app/src/main/java/edu/psu/swen888/primenumber/PrimeNumberActivity.java
@@ -0,0 +1,29 @@
+package edu.psu.swen888.primenumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class PrimeNumberActivity extends AppCompatActivity  {
+    private TextView mResultTextView;
+    private EditText mInputNumberEditText;
+    private Button mCheckIfPrimeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_prime_number);
+
+        mResultTextView = findViewById(R.id.result_text);
+        mInputNumberEditText = findViewById(R.id.number_field);
+        mCheckIfPrimeButton = findViewById(R.id.check_button);
+    }
+
+    public boolean checkIfPrim(int number){
+        return true;
+    }
+
+}
